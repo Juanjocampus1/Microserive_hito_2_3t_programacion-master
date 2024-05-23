538eaf58be3552243b653aed2d40c6e85368ce30
Juanjo <juanjose.acebedolara.23@campusfp.es>
2024-05-23 02:03:48 +0200

    añadimos los microservicios de jwt

---
+++ microservice-ProductManagement/src/main/java/com/empresa/microserviceProductManagement/MicroserviceProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.microserviceProductManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProductManagementApplication.class, args);
+	}
+
+}
---
+++ microservice-ProductManagement/src/test/java/com/empresa/microserviceProductManagement/MicroserviceProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.microserviceProductManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
